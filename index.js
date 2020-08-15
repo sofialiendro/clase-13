063b1db98391d2a5ee773a62552360badab69b92
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-15 15:19:00 -0400

    Ejercicio 1 y notas de la clase 13

---
+++ index.js
@@ -0,0 +1,34 @@
+// Esta es una alerta:
+// alert ("Hola, soy Sofía")
+
+// prompt ("Gatos o perros?")
+
+// confirm ("Te gustan los animales?")
+
+// let edad 
+
+// edad = 26
+
+// let AñoDeNacimiento = 1994
+
+// alert (edad)
+
+// edad = 25
+
+// alert (edad)
+
+// let respuestaDelUsuario = prompt("Decime tu nombre")
+
+// let saludo = "Hola, "
+
+// alert(saludo + respuestaDelUsuario)
+
+
+let nombre = prompt("Cuál es tu nombre?")
+
+let apellido = prompt("Cuál es tu apellido?")
+
+alert("Hola, " + nombre + ". Bienvenida a Ada!")
+
+
+
