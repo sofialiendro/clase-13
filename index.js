9aef9e3bb65e8f242be06aa5fe9f3274b169ea2e
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-15 19:18:17 -0400

    JS Clase 13

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 // edad = 26
 
-// let AñoDeNacimiento = 1994
+// let anioDeNacimiento = 1994
 
 // alert (edad)
 
@@ -24,11 +24,51 @@
 // alert(saludo + respuestaDelUsuario)
 
 
-let nombre = prompt("Cuál es tu nombre?")
+// let nombre = prompt("¿Cuál es tu nombre?")
 
-let apellido = prompt("Cuál es tu apellido?")
+// let apellido = prompt("¿Cuál es tu apellido?")
 
-alert("Hola, " + nombre + ". Bienvenida a Ada!")
+// alert("Hola, " + nombre + " " + apellido + ". ¡Bienvenida a Ada!")
 
 
+// Booleano
 
+const leGustaBailarAlUsuario = confirm ("Hacé clic en OK si te gusta bailar.")
+
+alert (leGustaBailarAlUsuario)
+
+console.log ("Hola, soy Sofía")
+
+console.log(1)
+
+let nombre = prompt("¿Cuál es tu nombre?")
+
+let apellido = prompt("¿Cuál es tu apellido?")
+
+let mensajeConComillasQueVamosAUsar = `Hola, ${nombre} ${apellido}.
+¡Bienvenida a Ada!`
+
+alert(mensajeConComillasQueVamosAUsar)
+
+console.log(typeof nombre)
+
+
+// Números
+
+let anioDeNacimiento = 1994
+
+let edad = 26
+
+let suma = anioDeNacimiento + edad
+
+let esMayor = anioDeNacimiento > edad
+
+console.log(esMayor)
+
+let cantidadDeAnios = prompt ("Decime tu edad.")
+
+console.log(cantidadDeAnios)
+
+let cantidadDeAniosComoNumero = Number(cantidadDeAnios)
+
+console.log(cantidadDeAniosComoNumero)
